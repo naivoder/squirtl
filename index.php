1c026e24113040f4812820382702afaf2dd41571
naivoder <credovia@students.kennesaw.edu>
2020-05-08 11:34:43 -0400

    inital commit, move to git

---
+++ index.php
@@ -0,0 +1,57 @@
+<div id="home">
+			<br><br>
+			<div class="w3-content w3-light-gray">
+			<div class="w3-row">
+				<div class="w3-twothird w3-left w3-padding-large">
+					<h1 align="left" class="w3-text-green w3-margin-left" style="position:relative;left:50px;text-shadow:2px 2px 0 #444;opacity:0.9;">
+						Squirtl protects
+						<br>
+						your birdfeeder.
+					</h1>
+
+				</div>
+				<div class="w3-third w3-left w3-padding-large">
+					<br><br>
+					 <iframe width="560" height="315"
+							 src="https://www.youtube.com/embed/o7LMY71nOSA?start=10" frameborder="0"
+							 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
+							 style="position:relative;bottom:50px;right:150px;">
+					</iframe>
+				</div>
+			</div>
+			<div class="w3-row w3-leftbar w3-card">
+				<div class="w3-container w3-padding-large w3-third" style="font-size:30px;background-color:white;height:270px;">
+					<h3 class="w3-text-green">
+						Intelligent
+					</h3>
+					<font size="5pt">
+						Using cutting edge computer vision algorithms, Squirtl is trained to identify and target unwanted guests,
+						unleashing a burst of water that leaves an unforgettable impression!
+					</font>
+				</div>
+				<div class="w3-container w3-padding-large w3-third" style="font-size:30px;background-color:white;height:270px;">
+					<h3 class="w3-text-green">
+						Safe
+					</h3>
+					<font size="5pt">
+						Squirtl only squirts squirells... keeping the furry pests away while your family, pets and the local birds stay nice and dry!
+					</font>
+				</div>
+				<div class="w3-container w3-padding-large w3-third" style="font-size:30px;background-color:white;height:270px;">
+					<h3 class="w3-text-green">
+						Humane
+					</h3>
+					<font size="5pt">
+						Squirtl is non-lethal, delivering a powerful, accurate deterrant without causing any harm to the squirells. The results are amusing, and ethical!
+					</font>
+				</div>
+			</div>
+			<br>
+			<div class="w3-container w3-center">
+				<font size="5pt">
+					Squirtl keeps a watchful eye over your birdfeeder, allowing birds to return and saving YOU a fortune on birdseed.
+				</font>
+			</div>
+			</div>
+			<hr>
+		</div>
