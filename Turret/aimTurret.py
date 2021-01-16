fbaa7ccd76b58ca6fe81ef8dd137267741dd8093
naivoder <naivoder@gmail.com>
2021-01-16 17:04:39 -0500

    remove excess

--- Turret/aimTurret.py
+++
@@ -1,8 +0,0 @@
-import serial
-arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-command = input()
-arduino.write(command)
-print(command)
-reachedPos = str(arduino.readline())
-#print(reachedPos)
-
